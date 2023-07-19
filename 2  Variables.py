1fe0ac1cdf880ff929621c29537faf4397cccc52
Maniraj S <138987866+Manirajcse@users.noreply.github.com>
2023-07-19 20:20:12 +0530

    Update and rename 1 Variables.py to 2  Variables.py

---
+++ 2  Variables.py
@@ -0,0 +1,39 @@
+x = 5
+y = "Mani"
+print(x)
+print(y)
+
+x = 4       # x is of type int
+x = "Raj" # x is now of type str
+print(x)
+
+#Casting
+x = str(3)    # x will be '3'
+y = int(3)    # y will be 3
+z = float(3)  # z will be 3.0
+
+#Get the type
+x = 5
+y = "Mani"
+print(type(x))
+print(type(y))
+
+#Single or Double Quotes?
+x = "Mani"
+# is the same as
+x = 'Raj'
+
+#Case-Sensitive
+#Variable names are case-sensitive.
+a = 4
+A = "Sagi"
+#A will not overwrite a
+
+
+#Legal variable names:
+myvar = "John"
+my_var = "John"
+_my_var = "John"
+myVar = "John"
+MYVAR = "John"
+myvar2 = "John"
