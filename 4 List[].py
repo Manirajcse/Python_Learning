2272632876352dea9a3f90203c788e403737b65c
Maniraj S <138987866+Manirajcse@users.noreply.github.com>
2023-07-19 20:30:00 +0530

    Create 4 List[].py

---
+++ 4 List[].py
@@ -0,0 +1,42 @@
+# List[], Tuples(), Set{}
+# List = []
+names = ['maniraj', 'priya', 'rubi', 'maniraj']
+print(names)
+
+#call particular value in list
+print(names[0])
+
+#slicing
+print(names[0:2])
+
+#negative slicing
+print(names[-4:-2])
+print(names[-4:-1])
+
+
+# change or update the list
+names[3] = 'subash'
+print(names)
+
+# methods - append
+names.append('nivi')
+print(names)
+
+
+# methods - sort
+
+#ascending
+no = [9, 6, 3, 5, 1]
+no.sort()
+print(no)
+
+#decending
+no.sort(reverse=True)
+print(no)
+
+#add two List[]
+addition = names + no
+print(addition)
+
+# length
+print(len(names))
