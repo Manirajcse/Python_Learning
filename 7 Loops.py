b6763e82021e8c439817da5aa79d3b5b07309d3b
Maniraj S <138987866+Manirajcse@users.noreply.github.com>
2023-07-19 20:40:36 +0530

    Create 7 Loops.py

---
+++ 7 Loops.py
@@ -0,0 +1,23 @@
+#loops
+
+name = 'mani'
+
+#for
+for hi in name:
+    print(hi)
+
+names = ['mani', 'hari', 'kathir']
+
+for xyz in names:
+    print(xyz)
+
+for i in "hey! how are u":
+    if i == '!':
+        print("! is present")
+    else:
+        print("! is not present")
+
+#range
+num = 7
+while num < 10:
+    print(num)
