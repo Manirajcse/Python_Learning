66b9b1a23ebc20ba168d4d925a6430d63eda1fa2
Maniraj S <138987866+Manirajcse@users.noreply.github.com>
2023-07-19 20:32:15 +0530

    Create 6 Tuples().py

---
+++ 6 Tuples().py
@@ -0,0 +1,22 @@
+#tuples - must use ()
+names = ('maniraj', 'hari', 'kathir', 'maniraj')
+# can't change values
+# sorting is also not working
+print(names)
+
+#call particular value in tuples
+print(names[0])
+
+
+#slicing
+print(names[0:2])
+
+# negative slicing
+print(names[-4:-2])
+print(names[-4:-1])
+
+# length
+print(len(names))
+
+# methods
+print(names)
