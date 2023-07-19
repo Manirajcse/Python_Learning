5c5f93667b34b03ec92c6e806b1b920f439f97c9
Maniraj S <138987866+Manirajcse@users.noreply.github.com>
2023-07-19 20:37:00 +0530

    Create 5 Set{}.py

---
+++ 5 Set{}.py
@@ -0,0 +1,18 @@
+#set - must use {}
+names = {'maniraj', 'hari', 'kathir'}
+#print(names)
+
+#we can add some values in set
+names.add('subash')
+#print(names)
+
+#dictionary - application of set
+#cannot be changed the data
+#store date like database
+#inside the dictionary - we can insert the 2 fields = "key": "value"
+
+data = {
+    "name": "mani",
+    "age": "25",
+}
+print(data.get("age"))
